13703055ae25564867877492976b0adbbe4f57e2
Soup <50962850+soupy-boy@users.noreply.github.com>
2026-07-11 17:17:38 -0600

    day 4 part 2 complete

---
+++ day_4/part_2/src/main.rs
@@ -0,0 +1,75 @@
+use std::{fs::File, io::{BufRead, BufReader, Result}};
+
+fn read_file(file_name: &str) -> Result<Vec<String>> {
+    let input_file = File::open(file_name)
+        .expect("couldn't read file");
+    let buf = BufReader::new(input_file);
+    return buf.lines().collect();
+}
+
+fn grid_val(grid: &Vec<Vec<u8>>, x: i64, y: i64) -> u8 {
+    if x < 0 || x >= grid.len() as i64 || y < 0 || y >= grid[0].len() as i64 {
+        // out of bounds
+        return 0;
+    }
+   return grid[x as usize][y as usize];
+}
+
+fn adjacent_under_four(grid: &Vec<Vec<u8>>, x: i64, y: i64) -> bool {
+    let val = grid_val(&grid, x - 1, y - 1) + grid_val(&grid, x, y - 1) + grid_val(&grid, x + 1, y - 1) +
+              grid_val(&grid, x - 1, y) + grid_val(&grid, x + 1, y) + 
+              grid_val(&grid, x - 1, y + 1) + grid_val(&grid, x, y + 1) + grid_val(&grid, x + 1, y + 1);
+    println!("grid[{x}][{y}]: {val}");
+    if val < 4 {
+        println!("under 4");
+        return true;
+    } else {
+        return false;
+    }
+}
+
+fn main() {
+    let mut removed: u64 = 0;
+    let lines = read_file("src/input.txt").unwrap();
+
+    let width = lines.first().unwrap().len();
+    let length = lines.len();
+
+    println!("width: {width}");
+    println!("length: {length}");
+
+    let mut grid: Vec<Vec<u8>> = Vec::new();
+    for line in &lines {
+        if line.is_empty() {
+            continue;
+        }
+
+        println!("{line}");
+
+        let int_line: Vec<u8> = line.chars().into_iter().map(|c| if c == '.' {0} else {1})
+            .collect();
+        println!("int_array: {:?}", int_line);
+
+        grid.push(int_line);
+        println!("grid: {:?}", grid);
+    }
+
+    let mut changed = true;
+    // loop through and remove paper until you can't anymore
+    while changed {
+        changed = false;
+        for row_index in 0..grid.len() {
+            for column_index in 0..grid[0].len() {
+                // check adjacent 8 positions, count if they add up to 4
+                // if so increment accessible
+                if grid[row_index][column_index] == 1u8 && adjacent_under_four(&grid, row_index as i64, column_index as i64) {
+                    removed += 1;
+                    grid[row_index][column_index] = 0;
+                    changed = true;
+                }
+            }
+        }
+    }
+
+    println!("accessible: {removed}");
+}
